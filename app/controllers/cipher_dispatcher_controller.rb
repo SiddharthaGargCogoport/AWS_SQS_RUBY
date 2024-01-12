667858074ef2529e9d3af419e66aebd06b16ee76
SiddharthaGargCogoport <Siddhartha.garg@cogoport.com>
2024-01-12 15:41:19 +0530

    added comments and debugged some test case errors

--- app/controllers/cipher_dispatcher_controller.rb
+++ app/controllers/cipher_dispatcher_controller.rb
@@ -2,17 +2,21 @@ class CipherDispatcherController < ApplicationController
     skip_before_action :verify_authenticity_token 
     def publish_message
       unique_number = SecureRandom.random_number(1000)
-      message_body = request.body.read
       sqs_client = Aws::SQS::Client.new
       queue_url = Rails.application.config.sqs_queue_url
-      # Generate a random number to use as a key for metadata
-     
-  
+      begin
+        message_body = request.body.read
       # Set up metadata as a message attribute
-      metadata = { "unique_number" => { data_type: 'String', string_value: unique_number.to_s } }
-  
-      sqs_client.send_message(queue_url: queue_url,message_body: message_body, message_attributes: metadata)
-  
-      render json: { unique_number: unique_number, body: message_body }
+        metadata = { "unique_number" => { data_type: 'String', string_value: unique_number.to_s } }
+        response = sqs_client.send_message({queue_url: queue_url,message_body: message_body, message_attributes: metadata})
+        rescue => error
+            response = {
+                status: error.http_code,
+                message: JSON.parse(error.response).to_h['message'],
+                exception: error.exception
+              }.to_json rescue { message: 'Request Failed' }.to_json
+        ensure
+        render json: { response: response,message_body: message_body,message_attributes: metadata }
+        end
     end
   end
--- app/controllers/infinity_interceptor_controller.rb
+++ app/controllers/infinity_interceptor_controller.rb
@@ -1,4 +1,4 @@
-class InfinityInterceptorController < AppplicationController 
+class InfinityInterceptorController < ApplicationController 
     skip_before_action :verify_authenticity_token 
     def receiver2
         if rand < 0.4
@@ -6,7 +6,7 @@ class InfinityInterceptorController < AppplicationController
         else
            result =  receiver1
         end
-    render json: { response: 'ok'}
+    render json: { response: result}
     end
 
 
@@ -18,19 +18,29 @@ class InfinityInterceptorController < AppplicationController
           poll_queue(queue_url)
           sleep poll_interval
         end
-      end
+    end
     
-      private
+      # private
     
       def poll_queue(queue_url)
         sqs_client = Aws::SQS::Client.new
-        queue_poller = Aws::SQS::QueuePoller.new(queue_url, client: sqs_client)
-        
-        queue_poller.poll(wait_time_second: 5) do |msg|
-         puts msg.body
-         unique_number = msg.attributes['unique_number']
-
-        # call to cipher_deceptor service here 
+        response = sqs_client.receive_message({
+            queue_url: queue_url, 
+            message_attribute_names: ["All"], # Receive all custom attributes.
+            max_number_of_messages: 4, # Receive at most 4 message.
+            wait_time_seconds: 5
+          })
+        response.messages.each do |msg|
+            Rails.logger.info msg.body
+            begin 
+              sqs_client.delete_message({queue_url: queue_url,receipt_handle: msg.receipt_handle})
+             rescue =>error
+              # logic to add processed msg to some to be deleted queue in database because it can't be deleted
+              Rails.logger.info 'Message deletion failed'
+             end
+          unique_number = msg.message_attributes['unique_number']
+          puts unique_number
+           # make the http call with header here
         end
       end
 end
